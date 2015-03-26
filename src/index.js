d70c7a4c0b9df08dd6002a641d2c232c29ad65ab
Kerri Shotts <kerrishotts@gmail.com>
2015-03-25 22:48:50 -0500

    Remove logging; fix up binds; and use promises within the class

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import "core-js/shim";
-import * as transforms from "transforms/index";
-let replace = require("replace"),
-    cordova = require("cordova-lib");
-
-function performSubstitutions(pkg) {
+import {transforms} from "./transforms/index";
+import cordovaLib from "cordova-lib";
+let cordova = cordovaLib.cordova.raw;
+import path from "path";
+function performSubstitutions(replace, pkg) {
     return replace({
         patterns: [
             {match: /{{{VERSION}}}/g, replacement: pkg.version},
@@ -13,18 +13,48 @@ function performSubstitutions(pkg) {
             {match: /{{{AUTHOR.NAME}}}/g, replacement: pkg.cordova.author.name},
             {match: /{{{AUTHOR.EMAIL}}}/g, replacement: pkg.cordova.author.email},
             {match: /{{{AUTHOR.SITE}}}/g, replacement: pkg.cordova.author.site},
-            {match: /{{{PREFS}}}/g, replacement: transforms.transformCordovaPrefs},
-            {match: /{{{ICONS}}}/g, replacement: transforms.transformCordovaAssets.bind(null, "icon", transforms.templates.CORDOVA_ASSET_TEMPLATE)},
-            {match: /{{{SPLASHES}}}/g, replacement: transforms.transformCordovaAssets.bind(null, "splash", transforms.templates.CORDOVA_ASSET_TEMPLATE)},
-            {match: /{{{GAP:PLUGINS}}}/g, replacement: transforms.transformCordovaPlugins},
-            {match: /{{{GAP:ICONS}}}/g, replacement: transforms.transformCordovaAssets.bind(null, "icon", transforms.templates.PGBUILD_ASSET_TEMPLATE)},
-            {match: /{{{GAP:SPLASHES}}}/g, replacement: transforms.transformCordovaAssets.bind(null, "splash", transforms.templates.PGBUILD_ASSET_TEMPLATE)}]
+            {match: /{{{PREFS}}}/g, replacement: transforms.transformCordovaPrefs.bind(null, pkg)},
+            {
+                match:       /{{{ICONS}}}/g,
+                replacement: transforms.transformCordovaAssets.bind(null, "icon", transforms.templates.CORDOVA_ASSET_TEMPLATE, pkg)
+            },
+            {
+                match:       /{{{SPLASHES}}}/g,
+                replacement: transforms.transformCordovaAssets.bind(null, "splash", transforms.templates.CORDOVA_ASSET_TEMPLATE, pkg)
+            },
+            {match: /{{{GAP:PLUGINS}}}/g, replacement: transforms.transformCordovaPlugins.bind(null, pkg)},
+            {
+                match:       /{{{GAP:ICONS}}}/g,
+                replacement: transforms.transformCordovaAssets.bind(null, "icon", transforms.templates.PGBUILD_ASSET_TEMPLATE, pkg)
+            },
+            {
+                match:       /{{{GAP:SPLASHES}}}/g,
+                replacement: transforms.transformCordovaAssets.bind(null, "splash", transforms.templates.PGBUILD_ASSET_TEMPLATE, pkg)
+            }]
     });
 }
 
-// cordova commands
-// inspired by https://github.com/kamrik/CordovaGulpTemplate/blob/master/gulpfile.js
-var cordovaTasks = {
+export const BUILD_MODE_RELEASE = "release",
+             BUILD_MODE_DEBUG   = "debug";
+
+export class CordovaTasks {
+    constructor({pkg, basePath, sourceDir, buildDir, gulp, replace}) {
+        this.pkg = pkg;
+        this.basePath = basePath;
+        this.sourcePath = path.join(basePath, sourceDir);
+        this.buildPath = path.join(basePath, buildDir);
+        this.gulp = gulp;
+        this.replace = replace;
+
+        this.performSubstitutions = performSubstitutions.bind(this, replace, pkg);
+    }
+
+    copyConfig() {
+        return this.gulp.src(path.join(this.sourcePath, "config.xml"))
+            .pipe(performSubstitutions(this.replace, this.pkg))
+            .pipe(this.gulp.dest(this.buildPath));
+    }
+
     /**
      * Creates the cordova project based on the settings in package.json
      * cordova.id specifies the app's id
@@ -33,107 +63,110 @@ var cordovaTasks = {
      *    TODO: maybe the above should be removed and www just deleted?
      *    TODO: that said, if we don't specify one, default gets downloaded
      */
-    create:        function create() {
-        gutil.log("Creating Cordova project at " + BUILD_DIR);
-        gutil.log("...  ID:", pkg.cordova.id);
-        gutil.log("...NAME:", pkg.cordova.name);
-        gutil.log("... SRC:", pkg.cordova.template);
-        return cordova.create(BUILD_DIR, pkg.cordova.id,
-            pkg.cordova.name, {
+    create() {
+        return cordova.create(this.buildPath, this.pkg.cordova.id,
+            this.pkg.cordova.name, {
                 lib: {
                     www: {
-                        url:  path.join(__dirname, pkg.cordova.template),
+                        url:  path.join(this.basePath, this.pkg.cordova.template),
                         link: false
                     }
                 }
-            })
-            .then(logDone);
-    },
+            });
+    }
+
     /**
      * All cordova commands other than create need to be in the project's
      * directory. This will change the current working directory to the
      * cordova project directory.
      */
-    cdProject:     function cdProject() {
-        process.chdir(path.join(BUILD_DIR, "www"));
-        gutil.log("... CWD:", process.cwd());
-    },
+    cdProject() {
+        process.chdir(path.join(this.buildPath, "www"));
+    }
+
     /**
      * Changes back to the (meta-) project's directory after a cordova
      * command
      */
-    cdUp:          function cdUp() {
+    cdUp() {
         process.chdir("..");
-        gutil.log("... CWD:", process.cwd());
-    },
+    }
+
     /**
      * Add the plugins specified in cordova.plugins in package.json
      */
-    addPlugins:    function addPlugins() {
-        gutil.log("Adding plugins...");
-        return cordova.plugins("add", pkg.cordova.plugins)
-            .then(logDone);
-    },
+    addPlugins() {
+        this.cdProject();
+        return cordova.plugins("add", this.pkg.cordova.plugins).then(this.cdUp);
+    }
+
     /**
      * Removes the plugins specified in cordova.plugins in package.json
      */
-    removePlugins: function removePlugins() {
-        gutil.log("Removing plugins...");
-        return cordova.plugins("rm", pkg.cordova.plugins.reverse())
-            .then(logDone);
-    },
+    removePlugins() {
+        this.cdProject();
+        return cordova.plugins("rm", this.pkg.cordova.plugins.reverse()).then(this.cdUp);
+    }
+
     /**
      * Adds the platforms specified in cordova.platforms in package.json.
      * The platforms must also be added as dev dependencies.
      */
-    addPlatforms:  function addPlatforms() {
+    addPlatforms() {
+        this.cdProject();
         function transformPlatform(platform) {
-            return path.join(__dirname, "node_modules", "cordova-" + platform);
+            return path.join(this.basePath, "node_modules", "cordova-" + platform);
         }
 
-        gutil.log("Adding platforms...");
-        return cordova.platforms("add", pkg.cordova.platforms.map(transformPlatform))
-            .then(logDone);
-    },
+        return cordova.platforms("add", this.pkg.cordova.platforms.map(transformPlatform.bind(this))).then(this.cdUp);
+    }
+
     /**
      * build the cordova project
      */
-    build:         function build() {
-        gutil.log("Building...", BUILD_MODE);
+    build({buildMode, options=[]}) {
+        this.cdProject();
         return cordova.build({
-            options: ["--" + BUILD_MODE]
-        })
-            .then(logDone);
-    },
+            options: ["--" + buildMode].concat(options)
+        }).then(this.cdUp);
+    }
+
     /**
      * prepare the cordova project
      */
-    prepare:       function prepare() {
-        gutil.log("Preparing...");
-        return cordova.prepare()
-            .then(logDone);
-    },
+    prepare() {
+        this.cdProject();
+        return cordova.prepare().then(this.cdUp);
+    }
+
     /**
      * emulate the app with the specified platform
      */
-    emulate:       function emulate() {
-        gutil.log("Starting emulator...", PLATFORM, BUILD_MODE);
+    emulate({platform, buildMode=BUILD_MODE_DEBUG}, options=[]) {
+        this.cdProject();
         return cordova.emulate({
-            platforms: [PLATFORM],
-            options:   ["--" + BUILD_MODE]
-        })
-            .then(logDone);
-    },
+            platforms: [platform],
+            options:   ["--" + buildMode].concat(options)
+        }).then(this.cdUp);
+    }
+
     /**
      * run the app on the specified platform
      */
-    run:           function run() {
-        gutil.log("Running app on device...", PLATFORM, BUILD_MODE);
+    run({platform, buildMode=BUILD_MODE_DEBUG, options=[]}) {
+        this.cdProject();
         return cordova.run({
-            platforms: [PLATFORM],
-            options:   ["--device", "--" + BUILD_MODE]
-        })
-            .then(logDone);
+            platforms: [platform],
+            options:   ["--device", "--" + buildMode].concat(options)
+        }).then(this.cdUp);
     }
-};
+
+    init() {
+        return this.create()
+            .then(this.copyConfig.bind(this))
+            .then(this.addPlugins.bind(this))
+            .then(this.addPlatforms.bind(this));
+    }
+
+}
 
